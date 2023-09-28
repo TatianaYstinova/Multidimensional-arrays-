75cb703202fb1f13174e28e79d306ab838e3b87b
ustin <rykova.tanya.96@gmail.com>
2023-09-28 14:26:00 +0300

    Add project files.

---
+++ Multidimensional arrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+int a = 2;
+int b = 6;
+int[,] array = new int [a,b];
+
+Random r = new Random();
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = r.Next(1, 10);
+    }
+}
+
+
+
+int[] Arr = new int[array.GetLength(0) * array.GetLength(1)];
+int index = 0;
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Arr[index++] = array[i, j];
+    }
+}
+
+int sum = 0;
+
+for (int i = 0; i < Arr.Length; i++)
+{
+    sum = sum + Arr[i];
+}
+
+Console.WriteLine(sum);
+
+
